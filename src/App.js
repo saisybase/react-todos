699abdf648f15dd3902012a699c4490eb03e9473
Sai Krishna <saikrishna.vallabhajoshyula@gmail.com>
2023-11-17 09:26:53 +0530

    sample commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div>
+          Your Todo's
+        </div>
+      <div>
+
+      <table>
+      <thead>
+      <tr>
+      <th scope="col1">#</th>
+      <th scope='col'>Description</th>
+      <th scope='col'>Assigned</th>
+      </tr>
+
+      </thead>
+
+      </table>
+
+
+      </div>
+
+      
+        <p>
+          we made our first react application
+        </p>
+      
+    </div>
+  );
+}
+
+export default App;
